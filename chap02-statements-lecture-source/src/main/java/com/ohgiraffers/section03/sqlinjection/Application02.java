ca424e4797dbcb54006ee1d033aab41c029dc9a6
kimkinghyeon <modan8199@gmail.com>
2024-06-18 14:22:44 +0900

    복습완

--- chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section03/sqlinjection/Application02.java
+++ chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section03/sqlinjection/Application02.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 import static com.ohgiraffers.config.JDBCTemplate.close;
 import static com.ohgiraffers.config.JDBCTemplate.getConnection;
 
-public class Application01 {
+public class Application02 {
     public static void main(String[] args) {
 
         // 회원 id 와 이름을 입력받고 두개가 일치하는 employee 가 있는지 확인하는 기능
 
         Connection con = getConnection();
 
-        Statement stmt = null;
+        PreparedStatement pstmt = null;
 
         ResultSet rset = null;
 
@@ -24,14 +24,18 @@ public class Application01 {
         System.out.print("회원 이름 입력 : ");
         String empName = sc.nextLine();
 
-        String query = "select * from employee where emp_id = '" + empId +
-                "' and emp_name = '" + empName + "'";
+        // 위치 홀더 를 이용해서 empId , empName 값을 모르기 떄문에?
+        String query = "select * from employee where emp_id = ? and emp_name = ?";
         System.out.println(query);
 
         try {
-            stmt = con.createStatement();
 
-            rset = stmt.executeQuery(query);
+            pstmt = con.prepareStatement(query);
+
+            pstmt.setString(1,empId); // 1, emp id = ?
+            pstmt.setString(2,empName); // 2,emp name = ?
+
+            rset = pstmt.executeQuery();
 
             if (rset.next()){
                 System.out.println(rset.getString("emp_name") + " 님 환영합니다. ");
@@ -42,7 +46,7 @@ public class Application01 {
             throw new RuntimeException(e);
         } finally {
             close(rset);
-            close(stmt);
+            close(pstmt);
             close(con);
         }
 
